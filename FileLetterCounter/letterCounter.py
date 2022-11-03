988e6c12da0dd44240462a580738f5903786956d
Ricky-RL <rickylin543@gmail.com>
2022-11-03 15:37:24 -0700

    added comments

---
+++ FileLetterCounter/letterCounter.py
@@ -0,0 +1,26 @@
+
+# Counts the amount of letters in a given text file and returns its dictionary
+def countLetters (fileName, fileType):
+    path = "C:\\Users\\iplay\\Documents\\intellj\\letterSoup\\textFiles"
+    if fileType == "book":
+        path += "\\books"
+    elif fileType == "movie":
+        path += "\\movies"
+
+    letters = generateAlphabetKeys()
+    with open(path + "\\" + fileName) as file:
+        for line in file:
+            for char in line:
+                if char in letters.keys():
+                    letters[char] += 1
+
+    return letters
+
+
+# Generates a dictionary for the alphabet, initializng all values to 0, returns dictionary
+def generateAlphabetKeys():
+    dict = {}
+    str = "abcdefghijklmnopqrstuvwxyz"
+    for c in str:
+        dict[c] = 0
+    return dict
